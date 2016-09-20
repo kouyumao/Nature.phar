963264eb1d6d8687faa29b9e1281eaa146ec6375
戴劼 <daijie@php.net>
2016-09-20 16:55:19 +0800

    cURL can send file now

--- cURL.php
+++ cURL.php
@@ -1,20 +1,20 @@
 <?php
     namespace Nature;
-    class cURL 
+    class cURL
     {
         private $ch;
-    
+
         function __construct()
         {
             $this->ch = curl_init();;
             curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
         }
-        
+
         function __setup($cfg)
         {
             curl_setopt($this->ch, CURLOPT_TIMEOUT, isset($cfg['timeout']) ? $cfg['timeout'] : 10);
         }
-        
+
         function processDataType($value, $type='txt')
         {
             switch($type){
@@ -34,24 +34,24 @@
                     return $value;
             }
         }
-        
+
         function errno()
         {
 	        return curl_errno($this->ch);
         }
-        
+
         function error()
         {
 	        return curl_error($this->ch);
         }
-        
+
         function getinfo($type)
         {
 	        $key = 'CURLINFO_'.strtoupper($type);
 	        $constant = constant($key);
 	        return curl_getinfo($this->ch, $constant);
         }
-        
+
         function setopt($key, $value=null)
         {
 	        if(is_array($key) && is_null($value)) {
@@ -61,27 +61,40 @@
 	        }
 	        return $this;
         }
-        
+
         function authorize($user, $password)
         {
             curl_setopt($this->ch, CURLOPT_USERPWD, $user.':'.$password);
 	        return $this;
         }
-        
+
         function get($url, $dataType='txt')
         {
             curl_setopt($this->ch, CURLOPT_URL, $url);
             $content = curl_exec($this->ch);
             return $this->processDataType($content, $dataType);
         }
-        
+
+        protected function hasFile($params=[])
+        {
+            foreach($params as $param) {
+                if(is_object($param) && is_a($param, 'CURLFile')) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
         function post($url, $params=[], $dataType='txt')
         {
-	        if(is_array($params)) {
-		        $params = http_build_query($params);
-	        }
             curl_setopt($this->ch, CURLOPT_URL, $url);
-            curl_setopt($this->ch, CURLOPT_POSTFIELDS, $params);
+
+            if(is_array($params) && !$this->hasFile($params)) {
+                $params = http_build_query($params);
+	        }
+            if(!empty($params)) {
+                curl_setopt($this->ch, CURLOPT_POSTFIELDS, $params);
+            }
             $content = curl_exec($this->ch);
             return $this->processDataType($content, $dataType);
         }
